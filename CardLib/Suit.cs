2367ce71924bad090c6aa4ecd9731544d56f9959
David Prause <praused@hotmail.com>
2017-05-29 13:23:18 -0400

    Chapter 10

--- CardLib/Suit.cs
+++ CardLib/Suit.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace CardLib
 {
-    public class Class1
+    public enum Suit
     {
+        Club,
+        Diamond,
+        Heart,
+        Spade
     }
 }
