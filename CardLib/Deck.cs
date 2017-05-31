22eaf9a2c5ebd3fa5c27024470f5dcdf39e4b371
David Prause <praused@hotmail.com>
2017-05-31 09:58:41 -0400

    Chapter 11, replaces cards array with a Cards collection

--- CardLib/Deck.cs
+++ CardLib/Deck.cs
@@ -7,16 +7,15 @@ namespace CardLib
 {
     public class Deck
     {
-        private Card[] cards;
+        private Cards cards = new Cards();
 
         public Deck()
         {
-            cards = new Card[52];
             for (int suitVal = 0; suitVal < 4; suitVal++)
             {
                 for (int rankVal = 1; rankVal < 14; rankVal++)
                 {
-                    cards[suitVal * 13 + rankVal - 1] = new Card((Suit)suitVal, (Rank)rankVal);
+                    cards.Add(new Card((Suit)suitVal, (Rank)rankVal));
                 }
             }
         }
@@ -32,24 +31,24 @@ namespace CardLib
 
         public void Shuffle()
         {
-            Card[] newDeck = new Card[52];
+            Cards newDeck = new Cards();
             bool[] assigned = new bool[52];
             Random sourceGen = new Random();
 
             for (int i = 0; i < 52; i++)
             {
-                int destCard = 0;
+                int sourceCard = 0;
                 bool foundCard = false;
                 while (foundCard == false)
                 {
-                    destCard = sourceGen.Next(52);
-                    if (assigned[destCard] == false)
+                    sourceCard = sourceGen.Next(52);
+                    if (assigned[sourceCard] == false)
                         foundCard = true;
                 }
-                assigned[destCard] = true;
-                newDeck[destCard] = cards[i];
+                assigned[sourceCard] = true;
+                newDeck.Add(cards[sourceCard]);
             }
-            newDeck.CopyTo(cards, 0);
+            newDeck.CopyTo(cards);
         }
     }
 }
