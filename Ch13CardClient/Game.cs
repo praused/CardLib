5374e14f76bccc4d219c9ba31f4484489f95575c
David Prause <praused@hotmail.com>
2017-09-11 18:11:13 -0400

    wip, through page 304

---
+++ Ch13CardClient/Game.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CardLib;
+
+namespace Ch13CardClient
+{
+    public class Game
+    {
+        private int currentCard;
+        private Deck playDeck;
+        private Player[] players;
+        private Cards discardedCards;
+
+        public Game()
+        {
+            currentCard = 0;
+            playDeck = new Deck(true);
+            playDeck.LastCardDrawn += new LastCardDrawnHandler(Reshuffle);
+            playDeck.Shuffle();
+            discardedCards = new Cards();
+        }
+
+        private void Reshuffle(Deck currentDeck)
+        {
+            Console.WriteLine("Discarded cards reshuffled into deck.");
+            currentDeck.Shuffle();
+            discardedCards.Clear();
+            currentCard = 0;
+        }
+
+        public void SetPlayers(Player[] newPlayers)
+        {
+            if (newPlayers.Length > 7)
+            {
+                throw new ArgumentException("A maximum of seven players may play this game.");
+
+            }
+            if (newPlayers.Length < 2)
+            {
+                throw new ArgumentException("A minimum of 2 players may play this game.");
+            }
+            players = newPlayers;
+        }
+
+        private void DealHands()
+        {
+            for (int p = 0; p < players.Length; p++)
+            {
+                for (int c = 0; c < 7; c++)
+                {
+                    players[p].PlayHand.Add(playDeck.GetCard(currentCard++));
+                }
+            }
+        }
+
+        public int PlayGame()
+        {
+            //Code to follow
+            return 1;
+        }
+
+    }
+}
